44f4ec4d92fc9ce8305803a2460a5f934f37dfe8
ctstat <ctstat@outlook.com>
2024-12-13 16:44:37 -0500

    add new tasks

--- cafe_rewards_offers.sql
+++ cafe_rewards_offers.sql
@@ -4,7 +4,12 @@ CREATE TABLE IF NOT EXISTS offers(
     difficulty INT,
     reward INT,
     duration INT,
-    channels TEXT
+    channels TEXT,
+    num_channels INT,
+    email INT,
+    mobile INT,
+    social INT,
+    web INT
 );
 
 CREATE TABLE IF NOT EXISTS customers(
@@ -19,12 +24,13 @@ CREATE TABLE IF NOT EXISTS events(
     customer_id VARCHAR(255),
     event VARCHAR(255),
     value TEXT,
-    time INT
+    time INT,
+    offer_id VARCHAR(255)
 );
 -----------------------------------------------------------------------
 -- import csv files
 COPY offers 
-FROM  'C:/Users/ctsta/Desktop/cafe_rewards_offers/offers.csv'
+FROM  'C:/Users/ctsta/Desktop/cafe_rewards_offers/clean_offers.csv'
 WITH (FORMAT CSV, HEADER TRUE)
 
 COPY customers 
@@ -32,7 +38,7 @@ FROM  'C:/Users/ctsta/Desktop/cafe_rewards_offers/customers.csv'
 WITH (FORMAT CSV, HEADER TRUE)
 
 COPY events 
-FROM  'C:/Users/ctsta/Desktop/cafe_rewards_offers/events.csv'
+FROM  'C:/Users/ctsta/Desktop/cafe_rewards_offers/clean_events.csv'
 WITH (FORMAT CSV, HEADER TRUE)
 
 -- check import files
@@ -49,7 +55,6 @@ FROM events
 LIMIT 3;
 
 --------------------------------------------------------------------------
--- unlist a column
 SELECT DISTINCT channels
 FROM offers;
 
@@ -91,3 +96,14 @@ WHERE income IS NULL;
 SELECT *
 FROM customer2
 
+---------------------------------------------------------------------------------
+-- join tables
+WITH cte_join AS (SELECT t1.customer_id, t1.event, t1.time, t2.*
+                 FROM events AS t1
+                 JOIN offers AS t2
+                 ON t1.offer_id = t2.offer_id)
+
+SELECT a.*, b.become_member_on, b.gender, b.age, b.income
+FROM cte_join AS a 
+JOIN customers AS b 
+ON a.customer_id = b.customer_id 
